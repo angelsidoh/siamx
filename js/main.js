f33ba6ccf604a7da9207267d2718a23033f5c1ab
unknown <angel._ruiz@hotmail.com>
2021-10-13 08:48:25 -0500

    modificacion inicial para congreso II

--- js/main.js
+++ js/main.js
@@ -1093,7 +1093,7 @@ $(function () {
 
 
 
-    $('.cuenta-regresiva').countdown('2021/01/08 17:00:00', function (event) {
+    $('.cuenta-regresiva').countdown('2022/01/08 17:00:00', function (event) {
         $('#dias').html(event.strftime('%D'));
         $('#horas').html(event.strftime('%H'));
         $('#minutos').html(event.strftime('%M'));
